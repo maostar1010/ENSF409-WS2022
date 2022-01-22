4d2f117173ee818320c6bdc1a821129fbeba3212
Ann Barcomb <ann.barcomb@ucalgary.ca>
2022-01-21 20:35:43 -0700

    Remaining files from this week.

---
+++ Assignment1/edu/ucalgary/ensf409/Itinerary.java
@@ -0,0 +1,33 @@
+package edu.ucalgary.ensf409;
+
+/*
+Copyright Ann Barcomb and Emily Marasco, 2021-2022
+All rights reserved.
+*/
+
+public class Itinerary {
+    private Trip[] trips = new Trip[20];
+
+    // Returns a string in the format of:
+    // value (key)
+    public static String fmtString(String key, String value) {
+    }
+
+    // Constructor
+    public Itinerary(String[][] myTrips) {
+    }
+
+    // Getter
+    public Trip[] getTrips() {
+    }
+
+    public String formatByArrival() {
+    }
+
+    // The first array holds years (2022-2023).
+    // The second array holds months.
+    // The third array holds formatLocation() fitting the criteria
+    String[][][] byDate() {
+    }
+
+}
