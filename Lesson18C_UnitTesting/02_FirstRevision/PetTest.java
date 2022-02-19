0a4a367cc8457978cd1191004b1294f129751823
Ann Barcomb <ann.barcomb@ucalgary.ca>
2022-02-18 18:23:18 -0700

    Adding lessons 15 and 18C (14 to come soon)

---
+++ Lesson18C_UnitTesting/02_FirstRevision/PetTest.java
@@ -0,0 +1,57 @@
+/*
+Copyright Ann Barcomb and Emily Marasco, 2022
+Licensed under GPL v3
+See LICENSE.txt for more information.
+*/
+
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PetTest {
+    
+    public PetTest() {
+    }
+    
+    @Test
+    public void testCalculateAge_BirthdayPast() {
+        
+        int[] birthDate = new int[]{2011,1,1};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 11;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+
+
+    @Test
+    public void testCalculateAge_BirthdayFuture() {
+        
+        int[] birthDate = new int[]{2011,7,1};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 10;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+
+    @Test
+    public void testCalculateAge_BirthdayToday() {
+        
+        int[] birthDate = new int[]{2011,2,17};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 11;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+	
+}
---
+++ Lesson18C_UnitTesting/04_Exceptions/PetTest.java
@@ -0,0 +1,78 @@
+/*
+Copyright Ann Barcomb and Emily Marasco, 2022
+Licensed under GPL v3
+See LICENSE.txt for more information.
+*/
+
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PetTest {
+    
+    public PetTest() {
+    }
+    
+    @Test
+    public void testCalculateAge_BirthdayPast() {
+        
+        int[] birthDate = new int[]{2011,1,1};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 11;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+
+
+    @Test
+    public void testCalculateAge_BirthdayFuture() {
+        
+        int[] birthDate = new int[]{2011,7,30};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 10;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+
+    @Test
+    public void testCalculateAge_BirthdayToday() {
+        
+        int[] birthDate = new int[]{2011,2,17};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 11;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }
+
+    @Test
+    public void testCalculateAge_BirthdayFuture2() {
+        
+        int[] birthDate = new int[]{2011,7,1};
+        
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+        
+        System.out.println("calculateAge");
+        int expResult = 10;
+        int result = petDog.calculateAge();
+        assertEquals("Calculated age was incorrect: ", expResult, result);
+    }	
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testCalculateAge_BirthdayException() {
+        
+        int[] birthDate = new int[]{2022,7,1};
+
+        Pet petDog = new Pet("Pongo", "Dog", "Dalmatian", 4, "Spotted", "Fur", birthDate);
+    }	
+}
+
