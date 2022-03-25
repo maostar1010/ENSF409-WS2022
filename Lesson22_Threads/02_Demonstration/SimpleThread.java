585dc75592d743f63cd1c407f0149d84a9f3b421
Ann Barcomb <ann.barcomb@ucalgary.ca>
2022-03-25 13:57:26 -0600

    Final release

---
+++ Lesson22_Threads/02_Demonstration/SimpleThread.java
@@ -0,0 +1,52 @@
+/*
+Copyright Ann Barcomb and Emily Marasco, 2021
+Licensed under GPL v3
+See LICENSE.txt for more information.
+*/
+package edu.ucalgary.ensf409;
+
+import java.lang.*;
+import java.util.Random;
+
+public class SimpleThread extends Thread {
+    public void run() {
+        // Delay the simplethread a random amount of time, then output
+        Random rand = new Random();
+        int delay = rand.nextInt(250);
+        try {
+            Thread.sleep(delay);
+            System.out.println("SimpleThread reached the output phase.");
+        }
+        catch (InterruptedException e) {
+            System.out.println("SimpleThread was interrupted from sleep.");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Main is starting iteration " + i);
+            SimpleThread st = new SimpleThread();
+            AdditionalThread at = new AdditionalThread();
+            st.start();
+            at.start();
+        }
+    }
+}
+
+class AdditionalThread extends Thread {
+    public void run() {
+        // Delay the AdditionalThread a random amount of time, then output
+        Random rand = new Random();
+        int delay = rand.nextInt(250);
+        try {
+            Thread.sleep(delay);
+            System.out.println("AdditionalThread reached the output phase.");
+        }
+        catch (InterruptedException e) {
+            System.out.println("AdditonalThread was interrupted from sleep.");
+        }
+    }
+}
+
+
